b96a00e775072339915da7638d6afe2006a6f4ef
adrianrob21 <89424134+adrianrob21@users.noreply.github.com>
2023-10-09 22:02:59 +0300

    [TASK] Add app colors / Change app name [v0.0.1] (#1)

--- source/App.js
+++ source/App.js
@@ -2,6 +2,10 @@ import React from "react";
 
 import Navigator from "Navigator";
 
-const App = () => <Navigator />;
+const App = () => (
+  <div className={"bg-darkGray w-full min-h-screen"}>
+    <Navigator />;
+  </div>
+);
 
 export default App;
