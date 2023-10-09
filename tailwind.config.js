dbeb93b88416d4e0bf52387402ae04dd15766989
Rob <adirobadi03@gmail.com>
2023-10-09 21:55:10 +0300

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
 export default {
   content: ["./public/index.html", "./source/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: "#d71a00",
+      darkGray: "#2f2f2f",
+      lightGray: "#c2c2c2",
+    },
   },
   plugins: [],
 };
