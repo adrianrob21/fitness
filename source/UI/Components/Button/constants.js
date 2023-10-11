1de41716c5941a5f10b3321f19c1ce88e9a6ac4e
Rob <adirobadi03@gmail.com>
2023-10-11 21:39:55 +0300

    Added button

---
+++ source/UI/Components/Button/constants.js
@@ -0,0 +1,7 @@
+export const BUTTON_VARIANTS = {
+  default: {
+    container:
+      "bg-primary hover:cursor-pointer pl-5 pr-5 pt-1 pb-1 rounded-3xl flex justify-center items-center",
+    text: "text-white",
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,10 @@ export default {
   content: ["./public/index.html", "./source/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      primary: "#d71a00",
       darkGray: "#2f2f2f",
       lightGray: "#c2c2c2",
+      primary: "#d71a00",
+      white: "#FFFFFF",
     },
   },
   plugins: [],
