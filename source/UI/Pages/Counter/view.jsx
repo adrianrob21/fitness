cf2a83839ac4a1e9117d3a42801cfc2ae8f242de
Rob <adirobadi03@gmail.com>
2023-10-28 02:52:12 +0300

    added disabled; and spinner

--- source/UI/Pages/Counter/view.jsx
+++ source/UI/Pages/Counter/view.jsx
@@ -1,22 +1,40 @@
-import React from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 import { Button } from "Components";
 
-const Counter = ({ count, decrement, increment, resetCounter }) => (
-  <>
-    <div
-      className={
-        "flex flex-col width-1/3 border justify-center items-center border-yellow-700"
-      }
-    >
-      <Button onClick={increment.bind(null, null)} label={"Increment"} />
-      <h1 className="text-4xl">{count}</h1>
-      <Button onClick={decrement.bind(null, null)} label={"Decrement"} />
-      <Button onClick={resetCounter.bind(null, null)} label={"Reset Counter"} />
-    </div>
-  </>
-);
+const Counter = ({ count, decrement, increment, resetCounter }) => {
+  const [processing, setProcessing] = useState(false);
+
+  const request = () => {
+    setProcessing(true);
+    setTimeout(() => {
+      setProcessing(false);
+    }, [1000]);
+  };
+
+  return (
+    <>
+      <div
+        className={
+          "flex flex-col width-1/3 border justify-center items-center border-yellow-700"
+        }
+      >
+        <Button processing={processing} onClick={request} label={"Increment"} />
+        <h1 className="text-4xl">{count}</h1>
+        <Button
+          disabled={true}
+          onClick={decrement.bind(null, null)}
+          label={"Decrement"}
+        />
+        <Button
+          onClick={resetCounter.bind(null, null)}
+          label={"Reset Counter"}
+        />
+      </div>
+    </>
+  );
+};
 
 Counter.propTypes = {
   count: PropTypes.number,
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,10 @@ export default {
       black: "#000000",
       darkGray: "#2f2f2f",
       lightGray: "#c2c2c2",
-      primary: "#d71a00",
+      primary: {
+        500: "#ff624d",
+        900: "#d71a00",
+      },
       white: "#FFFFFF",
     },
   },
