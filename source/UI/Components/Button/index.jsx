b3a22450ce80ff1f0a4c3344145d269712c408d7
adrianrob21 <89424134+adrianrob21@users.noreply.github.com>
2023-11-06 23:00:57 +0200

    [TASK] Add eslint and husky [v0.1.2] (#7)

--- source/UI/Components/Button/index.jsx
+++ source/UI/Components/Button/index.jsx
@@ -1,32 +1,30 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { faDumbbell } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PropTypes from 'prop-types';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { classNames, mock } from "Helpers";
+import { classNames, mock } from 'Helpers';
 
-import { BUTTON_VARIANTS } from "./constants";
+import { BUTTON_VARIANTS } from './constants';
 
 const Button = ({
   disabled = false,
   fullWidth = true,
-  label = "",
+  label = '',
   onClick = mock,
   processing = false,
-  variant = "default",
+  variant = 'default'
 }) => (
   <div
     onClick={disabled || processing ? mock : onClick}
     className={classNames(
       BUTTON_VARIANTS[variant].container,
-      fullWidth && "w-full",
-      disabled ? "bg-primary-500" : "bg-primary-900"
-    )}
-  >
+      fullWidth && 'w-full',
+      disabled ? 'bg-primary-500' : 'bg-primary-900'
+    )}>
     {!processing && <p className={BUTTON_VARIANTS[variant].text}>{label}</p>}
     {processing && (
       <div>
-        <FontAwesomeIcon icon={faDumbbell} spin style={{ color: "#ffffff" }} />
+        <FontAwesomeIcon icon={faDumbbell} spin style={{ color: '#ffffff' }} />
       </div>
     )}
   </div>
@@ -38,7 +36,7 @@ Button.propTypes = {
   label: PropTypes.string,
   onClick: PropTypes.func,
   processing: PropTypes.bool,
-  variant: PropTypes.string,
+  variant: PropTypes.string
 };
 
 export default Button;
