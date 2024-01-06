abc25e2aa252ab1d64a1dc8e600d88a3baa03429
Rob <adirobadi03@gmail.com>
2024-01-06 22:51:56 +0200

    created modal

---
+++ source/UI/Components/Modal/index.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+
+import { mock, classNames } from 'Helpers';
+
+const Modal = ({
+  modalName = '',
+  setVisible = mock,
+  visible = true,
+  children = null
+}) => {
+  return (
+    <>
+      <div
+        onClick={setVisible.bind(null, { [modalName]: false })}
+        className={classNames(
+          'absolute h-full w-full z-40 transition-all duration-700 hover:cursor-pointer',
+          !visible && 'opacity-30 hidden backdrop-blur-0',
+          visible && 'opacity-100 backdrop-blur-lg'
+        )}
+      />
+      <div
+        className={classNames(
+          'h-64 w-64 bg-black rounded-xl absolute left-0 z-50 right-0 mr-auto duration-200 transition-all ml-auto mt-auto mb-auto top-0 bottom-0',
+          !visible && 'scale-0',
+          visible && 'scale-100 visible'
+        )}>
+        {children}
+      </div>
+    </>
+  );
+};
+
+Modal.propTypes = {
+  children: PropTypes.any,
+  modalName: PropTypes.string,
+  setVisible: PropTypes.func,
+  visible: PropTypes.bool
+};
+
+export default Modal;
